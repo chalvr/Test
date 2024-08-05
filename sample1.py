5c098eb3deac564308f74def30ada0b4fc7eec2d
chalvr <chalvanthreddycm@gmail.com>
2024-08-05 18:09:32 -0500

    check again and again

--- sample1.py
+++ sample1.py
@@ -2,5 +2,6 @@ sample = input("Are you testing the sample:")
 if sample == "Yes":
     print("check it")
     print("Check Again")
+    print("Check Again and Again")
 else:
     print("Are continue doing what you are doing ")
