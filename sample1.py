3bd1e474717c94317833475b857747f2034d25a0
chalvr <chalvanthreddycm@gmail.com>
2024-08-05 17:49:37 -0500

    Sample1 changes were done

--- sample1.py
+++ sample1.py
@@ -1,1 +1,5 @@
-print("Just testing the git repo")
+sample = input("Are you testing the sample:")
+if sample == "Yes":
+    print("check it")
+else:
+    print("Are continue doing what you are doing ")
